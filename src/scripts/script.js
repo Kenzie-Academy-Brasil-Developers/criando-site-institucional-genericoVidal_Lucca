7c79ce38e8fe4edf01368405c41c2a038699a6de
Lucca Vidal <129102372+VidalLucca@users.noreply.github.com>
2023-05-29 01:07:07 -0300

    entrega

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,10 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal (){
+    const button= document.querySelector ('.subscribeButton')
+    const modalController = document.querySelector ('.modal__controller')
+button.addEventListener ('click', function() {
+    modalController.showModal()
+     
+})
+}
+handleModal()
